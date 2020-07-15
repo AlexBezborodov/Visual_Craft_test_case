71c08f8234738cce563476c2b96b921cbca2b188
ALex Bezborodov <alexbezborodov23@gmail.com>
2020-07-15 13:52:44 +0300

    add js function & finished project

--- app/js/index.js
+++ app/js/index.js
@@ -3,3 +3,20 @@ function toggleMobileMenu() {
     document.querySelector('.mobile-bar').classList.toggle('active')
     console.log('clicked');
 }
+
+
+
+
+function ShowInfo(){
+    let question = {
+        name : " example",
+        email: " example",
+        text: "example ",
+    };
+    var questionArray =[];
+    question.name = document.getElementById('input-name').value;
+    question.email = document.getElementById('Email').value;
+    question.text = document.getElementById('textarea-input').value;
+    questionArray.push(question);
+    console.log(questionArray[0]);  
+}
